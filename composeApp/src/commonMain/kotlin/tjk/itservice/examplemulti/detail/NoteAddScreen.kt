fd85c941012848c5940e06328de177224cbba44b
ismoil <ismoilhukumatov9990@gmail.com>
2025-11-15 16:58:15 +0500

    feat:Добавленно ui

--- composeApp/src/commonMain/kotlin/tjk/itservice/examplemulti/detail/NoteAddScreen.kt
+++ composeApp/src/commonMain/kotlin/tjk/itservice/examplemulti/detail/NoteAddScreen.kt
@@ -1,18 +1,36 @@
 package tjk.itservice.examplemulti.detail
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ArrowBackIosNew
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.arkivanov.decompose.extensions.compose.subscribeAsState
@@ -24,6 +42,8 @@ fun NoteAddScreen(
     modifier: Modifier = Modifier,
 ) {
     val state by component.model.subscribeAsState()
+    var nameNote by remember { mutableStateOf("") }
+    var descNote by remember { mutableStateOf("") }
 
     Scaffold(
         modifier = modifier,
@@ -36,15 +56,51 @@ fun NoteAddScreen(
                     }
                 }
             )
-        }
+        },
     ) { paddingValues ->
-        Column(
+
+        // Используем paddingValues от Scaffold, чтобы контент не ушел под TopAppBar
+        Box(
             modifier = Modifier
+                .fillMaxSize()
                 .padding(paddingValues)
-                .padding(32.dp),
-            verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
-            Text("AddScreen")
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState())
+                    .padding(16.dp)
+            ) {
+                Text("Имя заметки")
+                TextField(
+                    value = nameNote,
+                    onValueChange = { nameNote = it },
+                    label = { Text("Введите имя заметки") },
+                    modifier = Modifier.fillMaxWidth().padding(top = 9.dp)
+                )
+                Text("Описание",modifier.padding(top = 12.dp)
+                )
+                TextField(
+                    value = descNote,
+                    onValueChange = { descNote = it },
+                    label = { Text("Введите описания") },
+                    modifier = Modifier.fillMaxWidth().padding(top = 9.dp).height(80.dp)
+                )
+                Spacer(modifier = Modifier.height(80.dp))
+            }
+
+            Button(
+                onClick = { component.onBackPressed() },
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .fillMaxWidth()
+                    .padding(16.dp)
+                    .padding(WindowInsets.navigationBars.asPaddingValues())
+                    .height(60.dp)
+                    .imePadding()
+            ) {
+                Text("Сохранить")
+            }
         }
     }
 }
